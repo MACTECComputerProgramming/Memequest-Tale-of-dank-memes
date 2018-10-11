6b4694d430505aff4a80a4de6e0af304acbf0515
Dagan <20dcpenland@macon.k12.mo.us>
2018-10-11 11:06:23 -0500

    Unity One
    Upload Version 0.001 Adds Menu

---
+++ MemeQuest/Assets/menuSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menuSwitch : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Quit()
+    {
+
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}
