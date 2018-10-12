65da82f2ca601e9c482141a377d85b8d36912627
Dagan <20dcpenland@macon.k12.mo.us>
2018-10-12 11:02:33 -0500

    Dagan Menu back

---
+++ MemeQuestAttempt2/Assets/Scenes/MenuStuff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStuff : MonoBehaviour {
+
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene(1));
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+
+    }
+
+	
+}
