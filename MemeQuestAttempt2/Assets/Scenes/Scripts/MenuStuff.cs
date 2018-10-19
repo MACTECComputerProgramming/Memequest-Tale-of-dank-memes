988e0b0ea18767c0b9b2c2e765cd1302e574e77c
CS180 <CS180@RM18016-SD-0817.macon.k12.mo.us>
2018-10-19 10:31:39 -0500

    Xavier - Update: LevelManager
    Added a new level manager. Combat fields are 3x13

---
+++ MemeQuestAttempt2/Assets/Scenes/Scripts/MenuStuff.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStuff : MonoBehaviour {
+
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+
+    }
+
+    public void BackToTheMenu()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+	
+    //make skript to cause blackness should the slider get changed
+
+
+
+}
