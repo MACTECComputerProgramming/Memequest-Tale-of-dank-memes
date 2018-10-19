1020c41c06e5d44996d0fc988ffb41e47dc4e1cb
CS180 <CS180@RM18016-SD-0817.macon.k12.mo.us>
2018-10-19 09:28:25 -0500

    Xavier - WorkingTileMaps
    Got a working tile map. Each map is a 3x13 map.

---
+++ MemeQuestAttempt2/Assets/Scenes/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject tile;
+
+
+
+	public float TileSize{
+		get{ return tile.GetComponent<SpriteRenderer> ().sprite.bounds.size.x;}
+	}
+	// Use this for initialization
+	void Start () {
+		createLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//builds the actual areas that the characters battle in.
+	private void createLevel(){
+
+		Vector3 worldStart = Camera.main.ScreenToWorldPoint (new Vector3 (0,80 ));
+
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 13; x++) {
+				placeTile (x, y, worldStart);
+
+			}
+		}
+	}
+
+	//places the tiles in the needed position
+	private void placeTile(int x, int y, Vector3 worldStart){
+		GameObject newTile = Instantiate (tile);
+
+		newTile.transform.position = new Vector3 (worldStart.x + (TileSize * x),worldStart.y + (TileSize * y), 0);
+	}
+}
