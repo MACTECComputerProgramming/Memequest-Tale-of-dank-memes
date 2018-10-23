ca073dfd8978ede05449723591d0b36018c2594f
CS180 <CS180@RM18016-SD-0817.macon.k12.mo.us>
2018-10-23 10:25:38 -0500

    Xavier - Update
    Fixed sound problems and the combat scene break. Working on spawning entities/player

---
+++ MemeQuestAttempt2_2.0 - Copy/Assets/Scenes/CombatStuff/EventHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EventHandler : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+		HealthSystem healthSystem = new HealthSystem(100);
+
+        Debug.Log("health" + healthSystem.GetHealth());
+
+	}
+	
+	*/
+}
