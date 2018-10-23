0c5e3746cf0a5230769352c3fbecd6acec4cd302
CS180 <CS180@RM18016-SD-0817.macon.k12.mo.us>
2018-10-23 09:32:32 -0500

    possible help

---
+++ MemeQuestAttempt200 - Copy/Assets/Scenes/MenuStuff/optionMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class optionMenu : MonoBehaviour {
+
+  /*  public VolumePs()
+    
+    {
+        AudioListener.volume = 0;
+
+        AudioListener.volume = 1;
+
+        AudioListener.volume = .25f;
+
+        AudioListener.volume = 0.5f;
+
+    }
+*/
+
+
+
+     
+
+}
