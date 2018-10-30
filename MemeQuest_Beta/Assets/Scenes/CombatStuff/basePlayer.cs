d56c7a780c1d924b838225a2976670f82bf9df35
Zoe <20zlisaacson@macon.k12.mo.us>
2018-10-30 09:32:55 -0500

    ReAdded CombatScripts (Stop Removing/Losing Them)
    s,cripts added not attached yet also put sprites instead pf PHs for combat menu

---
+++ MemeQuest_Beta/Assets/Scenes/CombatStuff/basePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class basePlayer : MonoBehaviour  {
+
+
+    public string name;
+
+    public float baseHp = 100;
+    public float curHp;
+
+    public float  baseMp;
+    public float curMp;
+
+    public int stam;
+    public int intel;
+    public int dex;
+    public int agi;
+
+  //  bool isDead = false;
+  //  bool hurt;
+
+    public string current_health;
+    public string base_health;
+
+    public float HurtPlayer = 10;
+/*
+    void hurt()
+    {
+        curHp = curHp - HurtPlayer;
+    }
+
+    */
+   /* void Update()
+    {
+        current_health =  curHp.ToString();
+        base_health = baseHp.ToString();
+
+        Debug.Log(curHp);
+
+        if (curHp <= 0)
+        {
+            Debug.Log("Something died");
+
+        }
+    */
+    //}
+}
