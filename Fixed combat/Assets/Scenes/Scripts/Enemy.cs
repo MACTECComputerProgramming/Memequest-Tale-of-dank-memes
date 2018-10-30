333aa8fb86f9efa5da37ecad8a04e29d31cc9bc8
CS180 <CS180@RM18016-SD-0817.macon.k12.mo.us>
2018-10-30 11:04:48 -0500

    Xavier - Fixed point by
    Fixed some of the point by and now has a difficulty

---
+++ Fixed combat/Assets/Scenes/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField]
+	private int diff;
+
+	public int getDiff(){
+
+		return diff;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
