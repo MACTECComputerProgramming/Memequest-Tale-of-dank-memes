ad365e8948f10abe74074e186ebbbefb5ca4edba
Zoe <20zlisaacson@macon.k12.mo.us>
2018-11-01 09:06:16 -0500

    Sanics Attaces Added

---
+++ Fixed combat/Assets/Scenes/Scripts/Attacks/Sanic Attacks/Paunch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paunch : baseAttack {
+
+    public Paunch()
+    {
+        attackName = "Scratch";
+        attackDesc = "no desc";
+        attackDam = 5f;
+        attackCost = 0;
+
+
+        
+
+    }
+
+
+
+
+}
---
+++ Fixed combat/Assets/Scenes/Scripts/Attacks/Sanic Attacks/Supa Faust.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupaFaust : baseAttack {
+
+	public SupaFaust()
+    {
+        attackName = "Supa Faust";
+        attackDesc = "Takes actions as many times as their are enemies";
+        attackDam = 0f;
+        attackCost = 100;
+               
+
+    }
+}
